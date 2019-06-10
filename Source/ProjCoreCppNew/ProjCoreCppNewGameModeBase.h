a8c09c329a952a5771f5e354b5e0b041a9cf8957
alexey_eng <alexey_eng@mail.ru>
2019-06-10 17:23:06 +0500

    initial

---
+++ Source/ProjCoreCppNew/ProjCoreCppNewGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjCoreCppNewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJCORECPPNEW_API AProjCoreCppNewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
